f802739e3cde8894766d538b155239b66074475f
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-28 20:08:56 +0300

    Добавлена анимация на поле Ваше имя

--- markup/components/modal/modal.js
+++ markup/components/modal/modal.js
@@ -1,6 +1,33 @@
+/*eslint-disable*/
 $(document).on('click', '.contact-call-phone__link', function (e) {
     e.preventDefault();
     $('.modal').bPopup({
         closeClass: 'modal-close-link'
     });
 });
+
+var modalNameCustomer = document.querySelector('.name-customer-js');
+var superPlaceholderOptions = {
+    // delay between letters (in milliseconds)
+    letterDelay: 100, // milliseconds
+    // delay between sentences (in milliseconds)
+    sentenceDelay: 1000,
+    // should start on input focus. Set false to autostart
+    startOnFocus: true,
+    // loop through passed sentences
+    loop: true,
+    // Initially shuffle the passed sentences
+    shuffle: false,
+    // Show cursor or not. Shows by default
+    showCursor: true,
+    // String to show as cursor
+    cursor: '|'
+};
+
+superplaceholder({
+    el: modalNameCustomer,
+    sentences: ['Укажите Ваше имя', 'Например: Иван Иванов'],
+    options: superPlaceholderOptions
+});
+/*eslint-enable*/
+
