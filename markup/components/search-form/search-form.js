2a28e12168077b067ab6217b755de8fba63e4137
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-29 23:45:15 +0300

    Добавлена форма поиска товара

--- markup/components/search-form/search-form.js
+++ markup/components/search-form/search-form.js
@@ -1,21 +1,21 @@
 /*eslint-disable*/
-var setDeliveryObject = {
+var setSearchObject = {
     fiedlsetActive: 0,
-    $fieldsets: $('.step-delivery .step-delivery-form__fieldset'),
-    $inputPhone: $('.step-delivery .phone-js'),
-    $countProduct: $('.step-delivery .count-product-js'),
-    $btnForward: $('.step-delivery .btn-forward'),
-    $btnSubmit: $('.step-delivery .step-delivery-form .btn-submit'),
-    $stepDelivery: $('.step-delivery .step-delivery-form'),
-    $successBlock: $('.step-delivery .success-block'),
-    $deliveryBtn: $('.delivery-btn'),
-    $addressInput: $('.step-delivery .address-js')
+    $fieldsets: $('.search-product .search-product-form__fieldset'),
+    $inputPhone: $('.search-product .phone-js'),
+    $countProduct: $('.search-product .count-product-js'),
+    $btnForward: $('.search-product .btn-forward'),
+    $btnSubmit: $('.search-product .search-product-form .btn-submit'),
+    $stepDelivery: $('.search-product .search-product-form'),
+    $successBlock: $('.search-product .success-search-product-block'),
+    $deliveryBtn: $('.searching-btn'),
+    $productNameInput: $('.search-product .product-name-js')
 };
 
-setDeliveryObject.$countProduct.inputmask({ "mask": "9", "repeat": 4, "greedy": false });
-setDeliveryObject.$inputPhone.inputmask("+7(999)999-99-99");
+setSearchObject.$countProduct.inputmask({ "mask": "9", "repeat": 4, "greedy": false });
+setSearchObject.$inputPhone.inputmask("+7(999)999-99-99");
 
-setDeliveryObject.$addressInput.on('keyup', function(e){
+setSearchObject.$productNameInput.on('keyup', function(e){
     var $valThis = $(this).val();
     var $btnForward = $(this).closest('.field-row').find('.btn-forward');
     if($valThis.trim().length > 4){
@@ -24,7 +24,7 @@ setDeliveryObject.$addressInput.on('keyup', function(e){
         $btnForward.attr('disabled', 'disabled');
     }
 });
-setDeliveryObject.$inputPhone.on('keyup', function(e){
+setSearchObject.$inputPhone.on('keyup', function(e){
     var $btnForward = $(this).closest('.field-row').find('.btn-forward');
     if($(this).inputmask('isComplete')){
         $btnForward.removeAttr('disabled');
@@ -32,7 +32,7 @@ setDeliveryObject.$inputPhone.on('keyup', function(e){
         $btnForward.attr('disabled', 'disabled');
     }
 });
-setDeliveryObject.$countProduct.on('keyup', function(e){
+setSearchObject.$countProduct.on('keyup', function(e){
     var $btnSubmit = $(this).closest('.field-row').find('.btn-submit');
     var $thisValue = $(this).val();
     if($thisValue.length){
@@ -42,55 +42,55 @@ setDeliveryObject.$countProduct.on('keyup', function(e){
     }
 });
 
-setDeliveryObject.$deliveryBtn.on('click', function (e) {
+setSearchObject.$deliveryBtn.on('click', function (e) {
     e.preventDefault();
     $('.content').addClass('active');
-    setDeliveryObject.fiedlsetActive = 1;
+    setSearchObject.fiedlsetActive = 1;
 
     // Получаем ширину певого блока формы
-    var $widthFieldset = setDeliveryObject.$fieldsets.eq(setDeliveryObject.fiedlsetActive - 1).addClass('active').width();
+    var $widthFieldset = setSearchObject.$fieldsets.eq(setSearchObject.fiedlsetActive - 1).addClass('active').width();
     var $windowWidth = $(window).width();
     var $fieldToSetWidth = ($windowWidth / 2) - ($widthFieldset / 2);
-    setDeliveryObject.$fieldsets.eq(setDeliveryObject.fiedlsetActive - 1).animate({
+    setSearchObject.$fieldsets.eq(setSearchObject.fiedlsetActive - 1).animate({
         'left': $fieldToSetWidth + 'px'
     }, 200);
 });
-setDeliveryObject.$btnForward.on('click', function(e){
-    setDeliveryObject.fiedlsetActive = $(this).data('to-step');
-    $(this).closest('.step-delivery .step-delivery-form__fieldset').removeClass('active').addClass('diactive').animate({
+setSearchObject.$btnForward.on('click', function(e){
+    setSearchObject.fiedlsetActive = $(this).data('to-step');
+    $(this).closest('.search-product .search-product-form__fieldset').removeClass('active').addClass('diactive').animate({
         top: '-500vh'
     }, 500);
 
-    var $widthFieldset = setDeliveryObject.$fieldsets.eq(setDeliveryObject.fiedlsetActive - 1).width();
+    var $widthFieldset = setSearchObject.$fieldsets.eq(setSearchObject.fiedlsetActive - 1).width();
     var $windowWidth = $(window).width();
     var $fieldToSetWidth = ($windowWidth / 2) - ($widthFieldset / 2);
-    setDeliveryObject.$fieldsets.eq(setDeliveryObject.fiedlsetActive - 1).animate({
+    setSearchObject.$fieldsets.eq(setSearchObject.fiedlsetActive - 1).animate({
         'left': $fieldToSetWidth + 'px'
     }, 200).addClass('active');
 });
-setDeliveryObject.$btnSubmit.on('click', function(e){
+setSearchObject.$btnSubmit.on('click', function(e){
     e.preventDefault();
 
-    var formData = setDeliveryObject.$stepDelivery.serialize();
+    var formData = setSearchObject.$stepDelivery.serialize();
 
-    setDeliveryObject.$btnSubmit.closest(setDeliveryObject.$fieldsets).removeClass('active').addClass('diactive').animate({
+    setSearchObject.$btnSubmit.closest(setSearchObject.$fieldsets).removeClass('active').addClass('diactive').animate({
         top: '-500vh'
     }, 500);
 
-    var $successBlockWidth = setDeliveryObject.$successBlock.width();
+    var $successBlockWidth = setSearchObject.$successBlock.width();
     var $windowWidth = $(window).width();
     var $successBlockWidthSet = ($windowWidth / 2) - ($successBlockWidth / 2);
 
-    setDeliveryObject.$successBlock.addClass('active').delay(800).animate({
+    setSearchObject.$successBlock.addClass('active').delay(800).animate({
         left: $successBlockWidthSet + 'px'
     }, 200);
 });
 
 $(window).on('resize', function () {
     var $windowWidth = $(this).width();
-    var $fiedsetActive = $('.step-delivery .step-delivery-form__fieldset.active')
+    var $fiedsetActive = $('.search-product .search-product-form__fieldset.active')
     var $fieldSetActiveWidth = $fiedsetActive.width();
-    var $successBlockActive = $('.step-delivery .success-block.active');
+    var $successBlockActive = $('.search-product .success-search-product-block.active');
     var $successBlockActiveWidth = $successBlockActive.width();
 
     var $fieldSetActiveSet = ($windowWidth / 2) - ($fieldSetActiveWidth / 2);
